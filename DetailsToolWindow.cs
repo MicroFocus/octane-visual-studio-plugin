59ce422ba49dbe284f8fb6363906a128d17868b6
Ran, Ido <ido.ran@hpe.com>
2017-07-13 13:18:04 +0300

    Add DetailsToolWindow to present details of work item

--- DetailsToolWindow.cs
+++ DetailsToolWindow.cs
@@ -24,6 +24,8 @@ namespace Hpe.Nga.Octane.VisualStudio
     [Guid("d19915c9-3dea-4d5c-aa56-bd1fed3a7ab3")]
     public class DetailsToolWindow : ToolWindowPane
     {
+        private readonly OctaneToolWindowControl detailsControl;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="DetailsToolWindow"/> class.
         /// </summary>
@@ -34,12 +36,14 @@ namespace Hpe.Nga.Octane.VisualStudio
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            this.Content = new OctaneToolWindowControl();
+            detailsControl = new OctaneToolWindowControl();
+            this.Content = detailsControl;
         }
 
         internal void SetWorkItem(OctaneItemViewModel itemViewModel)
         {
             this.Caption = string.Format("Item #{0}", itemViewModel.ID);
+            detailsControl.DataContext = itemViewModel;
         }
     }
 }
