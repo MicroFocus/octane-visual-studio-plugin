73978199e4ee884f8c593d04ef274d19b0e797c4
Paul Ciobanu <paul.ciobanu@hpe.com>
2018-02-19 14:41:12 +0200

    Create test project

---
+++ octane-visual-studio-plugin-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MicroFocus.Adm.Octane.VisualStudio.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MicroFocus.Adm.Octane.VisualStudio.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03a92195-e4c0-4e3b-bcbc-c49d9e1170f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
