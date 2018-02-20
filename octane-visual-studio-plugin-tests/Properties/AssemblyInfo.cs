07eaaa8ad4c7f3e65639a806e0d93f637f64b087
Paul Ciobanu <paul.ciobanu@hpe.com>
2018-02-20 16:18:20 +0200

    Add MicroFocus licensing to all files

--- octane-visual-studio-plugin-tests/Properties/AssemblyInfo.cs
+++ octane-visual-studio-plugin-tests/Properties/AssemblyInfo.cs
@@ -1,12 +1,28 @@
+/*!
+* (c) 2016-2018 EntIT Software LLC, a Micro Focus company
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MicroFocus.Adm.Octane.VisualStudio.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MicroFocus")]
 [assembly: AssemblyProduct("MicroFocus.Adm.Octane.VisualStudio.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © MicroFocus 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
