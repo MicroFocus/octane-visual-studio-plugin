536217d4f29882483d91f016b16326b6a2df30d0
Paul Ciobanu <paul.ciobanu@hpe.com>
2018-06-06 11:36:58 +0300

    user story #563099: Add commit message tests for OctaneItemViewModel

--- octane-visual-studio-plugin-tests/ViewModel/OctaneItemViewModelTests.cs
+++ octane-visual-studio-plugin-tests/ViewModel/OctaneItemViewModelTests.cs
@@ -33,15 +33,18 @@ namespace MicroFocus.Adm.Octane.VisualStudio.Tests.ViewModel
     {
         private static Story _storyEntity;
         private static Task _taskEntity;
+        private static TestGherkin _gherkinTestEntity;
 
         private static OctaneItemViewModel _storyViewModel;
         private static OctaneItemViewModel _taskViewModel;
+        private static OctaneItemViewModel _gherkinTestViewModel;
 
         [ClassInitialize]
         public static void ClassInit(TestContext context)
         {
             _storyEntity = StoryUtilities.CreateStory();
             _taskEntity = TaskUtilities.CreateTask(_storyEntity);
+            _gherkinTestEntity = TestGherkinUtilities.CreateGherkinTest();
 
             var viewModel = new OctaneMyItemsViewModel();
             viewModel.LoadMyItemsAsync().Wait();
@@ -52,6 +55,9 @@ namespace MicroFocus.Adm.Octane.VisualStudio.Tests.ViewModel
             _taskViewModel = viewModel.MyItems.FirstOrDefault(i => i.ID == _taskEntity.Id && i.Entity.Name == _taskEntity.Name);
             Assert.IsNotNull(_taskViewModel, "Couldn't find task entity in MyWork");
 
+            _gherkinTestViewModel = viewModel.MyItems.FirstOrDefault(i => i.ID == _gherkinTestEntity.Id && i.Entity.Name == _gherkinTestEntity.Name);
+            Assert.IsNotNull(_gherkinTestViewModel, "Couldn't find gherkin entity in MyWork");
+
             Assert.IsNull(OctaneItemViewModel.CurrentActiveItem, "There shouldn't be an active item");
         }
 
@@ -60,6 +66,7 @@ namespace MicroFocus.Adm.Octane.VisualStudio.Tests.ViewModel
         {
             EntityService.DeleteById<Story>(WorkspaceContext, _storyEntity.Id);
             EntityService.DeleteById<Task>(WorkspaceContext, _taskEntity.Id);
+            EntityService.DeleteById<TestGherkin>(WorkspaceContext, _gherkinTestEntity.Id);
         }
 
         [TestMethod]
@@ -179,6 +186,28 @@ namespace MicroFocus.Adm.Octane.VisualStudio.Tests.ViewModel
             Assert.IsNull(OctaneItemViewModel.CurrentActiveItem, "There shouldn't be an active item after ClearActiveItem");
             Assert.IsFalse(_taskViewModel.IsActiveWorkItem, "Task item shouldn't be active after ClearActiveItem");
             Assert.IsFalse(_storyViewModel.IsActiveWorkItem, "Story item shouldn't be active after ClearActiveItem");
+        }
+
+        #endregion
+
+        #region CommitMessage
+
+        [TestMethod]
+        public void OctaneItemViewModelTests_CommitMessage_ForStory_Success()
+        {
+            Assert.AreEqual($"user story #{_storyEntity.Id}: ", _storyViewModel.CommitMessage, "Mismatched commit message");
+        }
+
+        [TestMethod]
+        public void OctaneItemViewModelTests_CommitMessage_ForTask_Success()
+        {
+            Assert.AreEqual($"user story #{_storyEntity.Id}: task #{_taskEntity.Id}: ", _taskViewModel.CommitMessage, "Mismatched commit message");
+        }
+
+        [TestMethod]
+        public void OctaneItemViewModelTests_CommitMessage_ForGherkinTest_Success()
+        {
+            Assert.AreEqual(string.Empty, _gherkinTestViewModel.CommitMessage, "Gherkin test doesn't support commit message");
         }
 
         #endregion
