ef53261b5e0899431f16469e11ae3240bdbab6a6
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-10-04 16:50:24 +0300

    user story #637008: creates dialog and adds message, needs to get formatted message tho

---
+++ OctaneVSPlugin/View/BusinessErrorDialog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MicroFocus.Adm.Octane.VisualStudio.ViewModel;
+using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicroFocus.Adm.Octane.VisualStudio.View
+{
+
+    /// <summary>
+    /// Interaction logic for BusinessErrorDialog.xaml
+    /// </summary>
+    public partial class BusinessErrorDialog : DialogWindow
+    {
+        private DetailedItemViewModel viewModel;
+
+        public BusinessErrorDialog(DetailedItemViewModel viewModel, Exception ex) : base()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            Owner = Application.Current.MainWindow;
+
+            errorMessage.Content = ex.Message;
+            this.viewModel = viewModel;
+        }
+
+        public void DoRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.RefreshCommand.Execute(sender);
+            this.Close();
+        }
+
+        public void OpenInBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.OpenInBrowserCommand.Execute(sender);
+            this.Close();
+        }
+
+        public void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
