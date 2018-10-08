b4f4f8adc920f5742e97b9d54d6e3c8bcf157b96
kdrtibor <kadar-tibor.manasse@hpe.com>
2018-10-08 17:22:34 +0300

    user story #637008: implements business error dialog box

--- OctaneVSPlugin/View/BusinessErrorDialog.xaml.cs
+++ OctaneVSPlugin/View/BusinessErrorDialog.xaml.cs
@@ -1,19 +1,24 @@
-﻿using MicroFocus.Adm.Octane.VisualStudio.ViewModel;
+﻿/*!
+* (c) 2016-2018 EntIT Software LLC, a Micro Focus company
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using MicroFocus.Adm.Octane.Api.Core.Connector.Exceptions;
+using MicroFocus.Adm.Octane.VisualStudio.ViewModel;
 using Microsoft.VisualStudio.PlatformUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MicroFocus.Adm.Octane.VisualStudio.View
 {
@@ -25,13 +30,17 @@ namespace MicroFocus.Adm.Octane.VisualStudio.View
     {
         private DetailedItemViewModel viewModel;
 
-        public BusinessErrorDialog(DetailedItemViewModel viewModel, Exception ex) : base()
+        public BusinessErrorDialog(DetailedItemViewModel viewModel, MqmRestException ex) : base()
         {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
             Owner = Application.Current.MainWindow;
 
             errorMessage.Content = ex.Message;
+            errorCode.Content = ex.ErrorCode;
+            httpStatus.Content = ex.StatusCode;
+            correlationId.Content = ex.CorrelationInfo;
+            stackTrace.Text = ex.StackTrace;
             this.viewModel = viewModel;
         }
 
