d0a09418dc482a81a1b637bc767c02e15cb30557
Andras Toth <atoth@microfocus.com>
2019-01-03 17:56:18 +0200

    Adding browser dialog for sso login

--- OctaneVSPlugin/View/BrowserDialog.xaml.cs
+++ OctaneVSPlugin/View/BrowserDialog.xaml.cs
@@ -1,5 +1,23 @@
-﻿using System;
+﻿/*!
+* (c) 2016-2018 EntIT Software LLC, a Micro Focus company
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using Microsoft.VisualStudio.PlatformUI;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +33,25 @@ using System.Windows.Shapes;
 
 namespace MicroFocus.Adm.Octane.VisualStudio.View
 {
-    /// <summary>
-    /// Interaction logic for BrowserDialog.xaml
-    /// </summary>
-    public partial class BrowserDialog : UserControl
-    {
-        public BrowserDialog()
-        {
-            InitializeComponent();
-        }
-    }
+
+	/// <summary>
+	/// Interaction logic for BrowserDialog.xaml
+	/// </summary>
+	public partial class BrowserDialog : DialogWindow
+	{
+		public string Url { get; set; }
+
+		public BrowserDialog(String Url)
+		{
+			InitializeComponent();
+			this.Url = Url;
+			browser.Navigate(new Uri(Url));
+		}
+
+		private void hyperlink_Click(object sender, RoutedEventArgs e)
+		{
+			// Open the URL in the user's default browser.
+			Process.Start(Url);
+		}
+	}
 }
