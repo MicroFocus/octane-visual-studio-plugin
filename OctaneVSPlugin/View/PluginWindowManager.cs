1ac4a4f4ab7c52e89b91fda8b27dcbbe9421cf55
Dan Moldoveanu <dan.moldoveanu@microfocus.com>
2023-05-24 11:40:09 +0300

    tech: opentext rebranding - change copyright

--- OctaneVSPlugin/View/PluginWindowManager.cs
+++ OctaneVSPlugin/View/PluginWindowManager.cs
@@ -1,5 +1,13 @@
 ﻿/*!
-* © Copyright 2017-2022 Micro Focus or one of its affiliates.
+* Copyright 2017 – 2023 Open Text.
+*
+* The only warranties for products and services of Open Text and
+* its affiliates and licensors (“Open Text”) are as may be set
+* forth in the express warranty statements accompanying such products
+* and services. Nothing herein should be construed as constituting an
+* additional warranty. Open Text shall not be liable for technical or
+* editorial errors or omissions contained herein. The information
+* contained herein is subject to change without notice.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
